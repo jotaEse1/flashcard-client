b12129f5bfa47e1a298af8a581a9c6243eda493b
Joaquin <joacosanchez20@gmail.com>
2023-01-11 13:19:16 -0300

    change urls

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,23 +1,23 @@
 export enum URL_AUTH {
-    SIGNIN = "http://localhost:5000/api/v1/auth/signin",
-    LOGIN = "http://localhost:5000/api/v1/auth/login",
-    LOGOUT = "http://localhost:5000/api/v1/auth/logout",
-    REFRESH_TOKEN = "http://localhost:5000/api/v1/auth/refresh_token"
+    SIGNIN = "https://flashcard-server-plpi.onrender.com/api/v1/auth/signin",
+    LOGIN = "https://flashcard-server-plpi.onrender.com/api/v1/auth/login",
+    LOGOUT = "https://flashcard-server-plpi.onrender.com/api/v1/auth/logout",
+    REFRESH_TOKEN = "https://flashcard-server-plpi.onrender.com/api/v1/auth/refresh_token"
 }
 
 export enum URL_DECK {
-    GET_DECKS = "http://localhost:5000/api/v1/deck/getDecks",
-    CREATE = "http://localhost:5000/api/v1/deck/create",
-    UPLOAD = "http://localhost:5000/api/v1/deck/upload",
-    DELETE = "http://localhost:5000/api/v1/deck/delete",
-    RENAME = "http://localhost:5000/api/v1/deck/rename",
-    ADD_WORD = "http://localhost:5000/api/v1/deck/addWord",
-    UPDATE_WORD = "http://localhost:5000/api/v1/deck/updateWord",
-    DELETE_WORD = "http://localhost:5000/api/v1/deck/deleteWord",
+    GET_DECKS = "https://flashcard-server-plpi.onrender.com/api/v1/deck/getDecks",
+    CREATE = "https://flashcard-server-plpi.onrender.com/api/v1/deck/create",
+    UPLOAD = "https://flashcard-server-plpi.onrender.com/api/v1/deck/upload",
+    DELETE = "https://flashcard-server-plpi.onrender.com/api/v1/deck/delete",
+    RENAME = "https://flashcard-server-plpi.onrender.com/api/v1/deck/rename",
+    ADD_WORD = "https://flashcard-server-plpi.onrender.com/api/v1/deck/addWord",
+    UPDATE_WORD = "https://flashcard-server-plpi.onrender.com/api/v1/deck/updateWord",
+    DELETE_WORD = "https://flashcard-server-plpi.onrender.com/api/v1/deck/deleteWord",
 }
 
 export enum URL_STUDY {
-    UPDATE =  "http://localhost:5000/api/v1/study/update"
+    UPDATE =  "https://flashcard-server-plpi.onrender.com/api/v1/study/update"
 }
 
 export enum MODAL {
